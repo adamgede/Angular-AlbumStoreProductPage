cd4e31b7902bd8c765c96b51872f9a4397c62adf
Adam Gedeborg <adamgedeborg@gmail.com>
2020-03-31 10:57:45 -0400

    Completed Module 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
 import { Observable } from 'rxjs/Observable';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
@@ -13,5 +14,10 @@ export class ProductService {
     return this._http.get(this._albumUrl).map(response => <Album>response.json());
   }
 
+  public getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
+  }
+
   private _albumUrl: string = "../assets/album.json";
+  private _productsUrl: string = "../assets/products.json";
 }
